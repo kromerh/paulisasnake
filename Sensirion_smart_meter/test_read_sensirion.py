a5384e827f23a80e516f10a6e4ed29d6f5947bd6
heiko.kromer@gmail.com <hkromer@hkyako.local>
2019-07-28 10:58:46 +0200

    test bluetooth connection

---
+++ Sensirion_smart_meter/test_read_sensirion.py
@@ -0,0 +1,26 @@
+import binascii
+import struct
+import pygatt.backends
+
+
+MACADDR='EF:F9:AA:8B:FC:94'
+TEMP_NOTI_UUID = '00002235-b38d-4985-720e-0f993a68ee41'
+HUMI_NOTI_UUID = '00001235-b38d-4985-720e-0f993a68ee41'
+
+adapter = pygatt.backends.GATTToolBackend()
+adapter.start()
+
+device = adapter.connect(MACADDR,5,'random')
+print("connected")
+
+temp = device.char_read(TEMP_NOTI_UUID)
+tempf = struct.unpack('<f',temp)
+print("read temp : %.2f" % tempf)
+
+
+humi = device.char_read(HUMI_NOTI_UUID)
+humif = struct.unpack('<f',humi)
+print("read humi : %.2f" % humif)
+
+device.disconnect()
+adapter.stop()
