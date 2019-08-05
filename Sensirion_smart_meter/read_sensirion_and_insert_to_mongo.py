6dcaac7dcc1a0170efdc26f769d80f12a7c13325
Heiko Kromer <heiko.kromer@gmail.com>
2019-08-05 19:33:53 +0200

    at deploy

--- Sensirion_smart_meter/read_sensirion_and_insert_to_mongo.py
+++ Sensirion_smart_meter/read_sensirion_and_insert_to_mongo.py
@@ -13,57 +13,52 @@ TEMP_NOTI_UUID = '00002235-b38d-4985-720e-0f993a68ee41'
 HUMI_NOTI_UUID = '00001235-b38d-4985-720e-0f993a68ee41'
 ADDRTYPE = pygatt.backends.BLEAddressType.random
 
-while True:
-	adapter = pygatt.backends.GATTToolBackend()
-	adapter.start()
-	connected = False
-	attempts = 5
 
-	for i in range(attempts):
-		if connected == False:
-			try:
-				device = adapter.connect(MACADDR,10,ADDRTYPE)
-				connected = True
-				print(f'connected: {connected}, attempt: {i}')
-				# device = adapter.connect(MACADDR)
-				# print("connected")
-
-				temp = device.char_read(TEMP_NOTI_UUID)
-				tempf = struct.unpack('<f',temp)
-				print(f'read temp {tempf}')
+adapter = pygatt.backends.GATTToolBackend()
+adapter.start()
+connected = False
+attempts = 5
 
 
-				humi = device.char_read(HUMI_NOTI_UUID)
-				humif = struct.unpack('<f',humi)
-				print(f'read humi {humif}')
+device = adapter.connect(MACADDR,10,ADDRTYPE)
+
+# device = adapter.connect(MACADDR)
+# print("connected")
+
+temp = device.char_read(TEMP_NOTI_UUID)
+tempf = struct.unpack('<f',temp)
+print(f'read temp {tempf}')
 
 
-				device.disconnect()
-				adapter.stop()
+humi = device.char_read(HUMI_NOTI_UUID)
+humif = struct.unpack('<f',humi)
+print(f'read humi {humif}')
 
-				# save in the MongoDB
-				# Choose the appropriate client
-				client = MongoClient()
 
-				# Connect to the test db
-				db = client.sensirion_test
+device.disconnect()
+adapter.stop()
 
-				# Use the employee collection
-				coll_test = db.temp_and_humid
+# save in the MongoDB
+# Choose the appropriate client
+client = MongoClient()
 
-				# ====== Inserting Documents ====== #
-				# Creating a simple Pandas DataFrame
-				time = pd.Timestamp.now()
-				data = {
-					'time': time,
-					'temp': tempf,
-					'humid': humif
-				       }
+# Connect to the test paulisasnake
+db = client.paulisasnake
 
-				df = pd.DataFrame(data)
-				# Bulk inserting documents. Each row in the DataFrame will be a document in Mongo
-				coll_test.insert_many(df.to_dict('records'))
-			except:
-				connected = False
-				print('Not connected, trying again')
-	sleep(100)
+# Use the employee collection
+coll_test = db.temp_and_humid
+
+# ====== Inserting Documents ====== #
+# Creating a simple Pandas DataFrame
+time = pd.Timestamp.now()
+data = {
+	'time': time,
+	'temp': tempf,
+	'humid': humif
+       }
+
+df = pd.DataFrame(data)
+# Bulk inserting documents. Each row in the DataFrame will be a document in Mongo
+coll_test.insert_many(df.to_dict('records'))
+
+
