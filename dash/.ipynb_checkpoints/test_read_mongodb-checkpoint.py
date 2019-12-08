437deebabf62344d9f13c0b66ea74e21f8bd539a
Heiko Kromer <heiko.kromer@gmail.com>
2019-12-08 19:32:11 +0100

    clean data step

---
+++ dash/.ipynb_checkpoints/test_read_mongodb-checkpoint.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pymongo import MongoClient
+
+
+# Choose the appropriate client
+client = MongoClient()
+
+# Connect to the db paulisasnake
+db = client.paulisasnake
+
+# Use the collection temp and humid
+coll = db.temp_and_humid
+
+# Bulk inserting documents. Each row in the DataFrame will be a document in Mongo
+# coll.insert_many(data.to_dict('records'))
+
+
+# ====== Finding Documents ====== #
+documents = coll.find().sort([('time', -1)]).limit(50)
+data = pd.DataFrame(list(documents))
+print(data)
