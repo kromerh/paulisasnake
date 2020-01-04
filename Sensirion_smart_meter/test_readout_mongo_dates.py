01ec60ae130c8143d0d322da8e207eeaf9a3a07a
Heiko Kromer <heiko.kromer@gmail.com>
2020-01-04 15:23:24 +0100

    test mongodb date

---
+++ Sensirion_smart_meter/test_readout_mongo_dates.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import datetime
+
+# Choose the appropriate client
+client = MongoClient()
+
+# Connect to the db
+db = client.paulisasnake
+
+# Use the collection
+coll = db.humid_storage
+
+agg = coll.humid_storage.aggregate([
+    {"$group": {
+        "_id" : { "$concat": [
+            {"$substr": [{"$year": "$time"}, 0, 4 ]},
+            "-",
+            {"$substr": [{"$month": "$time"}, 0, 2 ]},
+            "-",
+            {"$substr": [{"$dayOfMonth": "$time"}, 0, 2 ]},
+        ]},
+        "count": {"$sum": 1 }
+     }},
+     {"$sort": { "_id": 1 }}
+])
+
+print(agg)
