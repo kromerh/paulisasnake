071e88d7cf2a7f68c6e48996ed4f4e08b42ce3f1
Heiko Kromer <heiko.kromer@gmail.com>
2020-01-05 17:33:25 +0100

    test find dates

--- Sensirion_smart_meter/test_date_range.py
+++ Sensirion_smart_meter/test_date_range.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 import pandas as pd
 import numpy as np
-import datetime
 import dateutil
+from datetime import datetime, timedelta
+
+
 
 # Choose the appropriate client
 client = MongoClient()
@@ -13,14 +15,15 @@ db = client.paulisasnake
 # Use the collection
 coll = db.humid_storage
 
-
-
 start_date_str = "2019-12-11T00:00:00.000Z"
 end_date_str = "2019-12-14T00:00:00.000Z"
 
 start_date = dateutil.parser.parse(start_date_str)
 end_date = dateutil.parser.parse(end_date_str)
 
+end_date = end_date + timedelta(days=1)
+
+print(end_date)
 vals = coll.find({
 	"time": {
 		'$gte': start_date,
